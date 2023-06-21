e2852395e50eed62c9a30285280d3d108b0ae5a3
Galina684 <galina684@gmail.com>
2023-06-21 18:16:47 +0300

    Задачи 1-8

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-       //Задача 1
+        System.out.println("Задача 1");//Задача 1
         int in = 5000000;
         byte by = 100;
         short sh = 1000;
@@ -14,7 +14,7 @@ public class Main {
         System.out.println(lo);
         System.out.println(fl);
         System.out.println(db);
-        //Задача 2
+        System.out.println("Задача 2"); //Задача 2
         float fl1 = 27.12f;
         long lo1 = 987678965549L;
         double db1 = 2.786;
@@ -29,13 +29,13 @@ public class Main {
         System.out.println(sh2);
         System.out.println(in1);
         System.out.println(by1);
-        //Задача 3
-        int lp = 23;
-        int as = 27;
-        int ea = 30;
-        int paper = 480 / (lp + as + ea);
+        System.out.println("Задача 3"); //Задача 3
+        int lyudmilaPav = 23;
+        int annaSer = 27;
+        int ekaterinaAnd = 30;
+        int paper = 480 / (lyudmilaPav + annaSer + ekaterinaAnd);
         System.out.println("На каждого ученика рассчитано " + paper + " листов бумаги");
-        //Задача 4
+        System.out.println("Задача 4"); //Задача 4
         int min2 = 16;
         int min1 = min2 / 2;
         int min20 = 20 * min1;
@@ -46,7 +46,7 @@ public class Main {
         System.out.println("За сутки машина произвела " + day1 + " штук бутылок");
         System.out.println("За 3 дня машина произвела " + day3 + " штук бутылок");
         System.out.println("За месяц машина произвела " + day30 + " штук бутылок");
-        //Задача 5
+        System.out.println("Задача 5"); //Задача 5
         int allCansPaint = 120;
         int whitePaint1 = 2;
         int brownPaint1 = 4;
@@ -55,7 +55,45 @@ public class Main {
         int brownPaintAll = brownPaint1 * classRooms;
         System.out.println("В школе, где " +  classRooms + " классов, нужно "
                   + whitePaintAll + " банок белой краски и " + brownPaintAll + " банок коричневой краски");
-
+        System.out.println("Задача 6");//Задача 6
+        int banana = 80;
+        int milk = 105;
+        int iceCream = 100;
+        int egg = 70;
+        int breakfastGr = banana*5 + milk*2 + iceCream*2 + egg*4;
+        int kg1 = 1000;
+        float breakfastKg = breakfastGr/(float)kg1;
+        System.out.println("Завтрак спортсмена весит " + breakfastGr + " гр");
+        System.out.println("Завтрак спортсмена весит " + breakfastKg + "кг");
+        System.out.println("Задача 7"); //Задача 7
+        int kg7 = kg1*7;
+        int diet250 = 250;
+        int diet500 = 500;
+        int result250 = kg7/diet250;
+        int result500 = kg7/diet500;
+        int resultMean = (result250 + result500)/2;
+        System.out.println("При потери веса по 250 грамм в день спортсмен похудеет на 7 кг за " + result250 + " дней");
+        System.out.println("При потери веса по 500 грамм в день спортсмен похудеет на 7 кг за " + result500 + " дней");
+        System.out.println("В среднем спортсмену понадобится " + resultMean + " дней, чтобы похудеть на 7 кг");
+        System.out.println("Задача 8"); //Задача 8
+        int mashaPay = 67760;
+        int denisPay = 83690;
+        int kristinaPay = 76230;
+        int mashaPayNew = mashaPay*10/100 + mashaPay;
+        int mashaPayLastY = mashaPay * 12;
+        int mashaPayNewY = mashaPayNew * 12;
+        int mashaProfit = mashaPayNewY - mashaPayLastY;
+        System.out.println("Маша теперь получает " + mashaPayNew + " рублей. Годовой доход вырос на " + mashaProfit);
+        int denisPayNew = denisPay*10/100 + denisPay;
+        int denisPayLastY = denisPay * 12;
+        int denisPayNewY = denisPayNew * 12;
+        int denisProfit = denisPayNewY - denisPayLastY;
+        System.out.println("Денис теперь получает " + denisPayNew + " рублей. Годовой доход вырос на " + denisProfit);
+        int kristinaPayNew = kristinaPay*10/100 + kristinaPay;
+        int kristinaPayLastY = kristinaPay * 12;
+        int kristinaPayNewY = kristinaPayNew * 12;
+        int kristinaProfit = kristinaPayNewY - kristinaPayLastY;
+        System.out.println("Кристина теперь получает " + kristinaPayNew + " рублей. Годовой доход вырос на " + kristinaProfit);
 
 
     }
